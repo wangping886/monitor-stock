b41d87be83d1dc35c012d82df5f9146eb10039fc
c.wangping <wangping@cndatacom.com>
2025-02-18 14:07:08 +0800

    init commit

--- xueqiu.py
+++ xueqiu.py
@@ -18,26 +18,31 @@ def getPriceList(codes,dataMap):
         percent = data['percent']
         low =(data['low']-data['last_close'])*100/data['last_close']
         low=round(low, 2) #保留两位
+        high =(data['high']-data['last_close'])*100/data['last_close']
+        high=round(high, 2) #保留两位
         # 格式化输出股票名称和股票价格
-        print(f'{stock_name} 的涨幅是: {percent},最低点位：{low}')
+        print(f'{stock_name} 的涨幅是: {percent},最低点位：{low},最高点位：{high}')
 
         
 ball.set_token("xq_a_token=e9373e1b65ec0f1e6706060d6d5cfca055a5e3fd")
 
 #print(json.dumps(ball.quotec('SH600000'), indent=4))#获取某支股票的行情数据
 
-stockMap = {'SH600011': '华能国际','SH601995': '中金公司',#A50权重
+stockMap = {'SH600011': '华能国际','SH601995': '中金公司','SH601398':'工商银行',#A50权重
 'SH688981': '中芯国际','SH688256': '寒武纪',#科创权重
 'SZ300750': '宁德时代','SZ300059': '东方财富',#创业权重
-'SZ002640': '跨境通','SH600133': '桐昆股份',}
-stock_codes = list(stockMap.keys())
+}
+stockCodes = list(stockMap.keys())
+
+cosumeMap={'SH600809':'山西汾酒','SH600132':'重庆啤酒','SZ000729':'燕京啤酒','SH600600':'青岛啤酒'}
+cosumeCodes = list(cosumeMap.keys())
 
 fundMap ={ 'SH588000': '科创50ETF','SH510050': 'A50ETF', 'SH510300': '沪深300','SZ159915':'创业板ETF'}
-fund_codes = list(fundMap.keys())
+fundCodes = list(fundMap.keys())
 
-getPriceList(stock_codes,stockMap)
-print(f'=================')
-getPriceList(fund_codes,fundMap)
+getPriceList(cosumeCodes,cosumeMap)
+getPriceList(stockCodes,stockMap)
+getPriceList(fundCodes,fundMap)
 
 
 
